0264a4937e30af73b9849064eba405ff6eafd0ae
jinjia1234 <jinjia1234@126.com>
2022-10-26 06:20:54 +0800

    git

--- hook.cmd
+++ hook.cmd
@@ -1,21 +1,19 @@
 @echo off
-ECHO 完全路径：%0
-ECHO 去掉引号：%~0
-ECHO 所在分区：%~d0
-ECHO 所处路径：%~p0
-ECHO 文件名：%~n0
-ECHO 扩展名：%~x0
-ECHO 文件属性：%~a0
-ECHO 修改时间：%~t0
-ECHO 文件大小：%~z0
-echo 当前的盘符:%~d0
-echo 当前的盘符及路径:%~dp0
-echo 当前的盘符及路径的短文件名格式:%~sdp0
-echo 当前批处理文件名:%~n0
-echo.
-cls
-
+setlocal enabledelayedexpansion
 git fetch --all
 git reset --hard origin/master
-git pull --rebase origin/master
+git pull --rebase origin master
+
+set port=14814
+for /f "tokens=1-5" %%a in ('netstat -ano^|findstr ":%port%"') do (
+    echo %%a %%b %%c %%d %%e
+    if "%%e%" == "" (
+        taskkill /f /pid %%d
+        set pid=%%d
+    ) else (
+        taskkill /f /pid %%e
+        set pid=%%e
+    )
+)
+
 go run main.go
